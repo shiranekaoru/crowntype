4f5793e47d0999b2c0c71d43da1b5ad53ff65148
shiranekaoru <shirane@iplab.cs.tsukuba.ac.jp>
2023-10-06 22:49:45 +0900

    Initial Commit

---
+++ crown_type Watch App/crown_typeApp.swift
@@ -0,0 +1,17 @@
+//
+//  crown_typeApp.swift
+//  crown_type Watch App
+//
+//  Created by shirane kaoru on 2023/10/06.
+//
+
+import SwiftUI
+
+@main
+struct crown_type_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ crown_type/crown_typeApp.swift
@@ -0,0 +1,17 @@
+//
+//  crown_typeApp.swift
+//  crown_type
+//
+//  Created by shirane kaoru on 2023/10/06.
+//
+
+import SwiftUI
+
+@main
+struct crown_typeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
